a493b2236da2591d83414ad5d4abdc6015d33591
Nedim Kulovac <nedim.kulovac@gmail.com>
2021-02-28 20:56:22 +0100

    minor change: animation added to image on product detail paige

--- resources/views/shop/show.blade.php
+++ resources/views/shop/show.blade.php
@@ -10,10 +10,36 @@
   </div>
   <div class="w3-col w3-container m10 l10  w3-card">
     <div class="w3-col w3-container m5 l5">
-      <div class="" style="width:100%; max-height:500px; overflow: hidden;">
-        <img style="object-fit: scale-down; object-position: center; max-height: 500px; width: 100%;" src="{{ productImage($product->image) }}" alt="{{ $product->name }}" style="width:100%">
+      @if ($product->images)
+      <div class="" style="width:100%; max-height:800px; overflow: hidden;">
+        <div class="w3-content" style="width: 100%">
+          @foreach(json_decode($product->images, true) as $image)
+          @if($loop->index == 0)
+          <img class="productSlides w3-animate-bottom" src="{{ productImage($image) }}" style="width:100%;">
+          @else
+          <img class="productSlides w3-animate-bottom" src="{{ productImage($image) }}" style="width:100%;display:none">
+          @endif
+          @endforeach
+          <div class="w3-row-padding w3-section">
+            @foreach(json_decode($product->images, true) as $image)
+            @if($loop->index == 0)
+            <div class="w3-col s4">
+              <img class="thumb w3-border-blue w3-bottombar" src="{{ productImage($image) }}" style="padding: 3px 0; width:100%;cursor:pointer; object-fit: scale-down; object-position: center; max-height: 100px; width: 100%;" onclick="currentSlide({{ $loop->index + 1 }})">
+            </div>
+            @else
+            <div class="w3-col s4">
+              <img class="thumb w3-border-light-grey w3-bottombar" src="{{ productImage($image) }}" style="padding: 3px 0; width:100%; cursor:pointer; object-fit: scale-down; object-position: center; max-height: 100px; width: 100%;" onclick="currentSlide({{ $loop->index + 1 }})">
+            </div>
+            @endif
+            @endforeach
+          </div>
+        </div>
       </div>
+      @else
+      <img style="object-fit: scale-down; object-position: center; max-height: 500px; width: 100%;" src="{{ productImage($product->image) }}" alt="{{ $product->name }}" style="width:100%">
+      @endif
     </div>
+
     <div class="w3-col w3-container m5 l5 ">
       <h1 style="font-weight: bold;">{{ $product->name }}</h1>
       <div style="margin-top: 35px;">
@@ -66,4 +92,37 @@
   </div>
 </div>
 
+@endsection
+
+@section('extra-js')
+<script>
+  function currentSlide(n) {
+    showSlide(slideIndex = n);
+  }
+
+  function showSlide(n) {
+    var i;
+    var slides = document.getElementsByClassName("productSlides");
+    var thumbs = document.getElementsByClassName("thumb");
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+      slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < thumbs.length; i++) {
+
+      if (thumbs[i].classList.contains("w3-border-blue")) {
+        thumbs[i].className = thumbs[i].className.replace(" w3-border-blue", " w3-border-light-grey");
+      }
+    }
+    slides[slideIndex - 1].style.display = "block";
+    if (thumbs[slideIndex - 1].classList.contains("w3-border-light-grey")) {
+      thumbs[slideIndex - 1].className = thumbs[slideIndex - 1].className.replace("w3-border-light-grey", "w3-border-blue");
+    }
+  }
+</script>
 @endsection
